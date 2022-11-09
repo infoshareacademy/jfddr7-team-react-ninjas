9f3c57b38f9e1e927a26af3b8cf4817a41b0d6a1
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-09 14:29:29 +0100

    Firebase topic - angielski works

--- hs-notes/src/components/SubjectNotes/SubjectNotes.tsx
+++ hs-notes/src/components/SubjectNotes/SubjectNotes.tsx
@@ -1,12 +1,36 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { db } from "../../firebase";
 
 export const SubjectNotes = () => {
     const params = useParams();
-    console.log(params);
+    
+    const [topicList, setTopicList] = useState(['']);
+    const downloadData = async () => {
+      getDocs(collection(db, '/Subjects/2VyggQ76uMI3CzPnwzUx/Topics')).then((querySnapshot) => {
+        let topics: string[] = [];
+         querySnapshot.forEach((doc) => {
+            topics.push(doc.data().Topic);
+         })
+        setTopicList(topics);
+      })
+   }
+   console.log(topicList);
+   
+   useEffect(() => {
+    downloadData();
+   }, [])
+   
     
     return (
         <div>
             <h1>Notatki z {params.id}</h1>
+            <div className="topic-list">
+                {topicList.map((item, number) => (
+                    <div key={number}>{item}</div>
+                ))}
+            </div>
         </div>
     )
 }
