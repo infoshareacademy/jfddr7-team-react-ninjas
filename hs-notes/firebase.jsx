0818ca1e1df5a3be853b0c5ea25346d78d6987f9
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-09 09:14:32 +0100

    Firebase innit

---
+++ hs-notes/firebase.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {getAuth} from 'firebase/auth';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXDixMFG0bWox72Yyg6hU7_DklCSLIIKw",
+  authDomain: "hs-notes-5a863.firebaseapp.com",
+  projectId: "hs-notes-5a863",
+  storageBucket: "hs-notes-5a863.appspot.com",
+  messagingSenderId: "511050791083",
+  appId: "1:511050791083:web:28b5aa6b6b41e31914dfee",
+  measurementId: "G-XYZ63Q9NZG"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
