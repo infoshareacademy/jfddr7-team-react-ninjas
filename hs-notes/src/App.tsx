a2485e6d47161f00a9106117e973ffc8ef58ac73
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-09 12:54:02 +0100

    Getting docs from firebase added

--- hs-notes/src/App.tsx
+++ hs-notes/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Register } from './components/Register/Register';
 import { Routes, Route } from 'react-router-dom';
 import { Login } from './components/Login';
+import { Subjects } from './components/Subjects/Subjects';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       </Route>
       <Route path='/register' element={<Register />}>
       </Route>
-      <Route path='/home' element={<Register />}>
+      <Route path='/home' element={<Subjects />}>
       </Route>
     </Routes>
     </div>
