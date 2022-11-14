29be0e0da87d181f9b5c2c91ef60dfe171a5d6fb
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-14 12:26:58 +0100

    Downloading notes from firebase working

--- hs-notes/src/components/NoteList/NoteList.tsx
+++ hs-notes/src/components/NoteList/NoteList.tsx
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, collectionGroup, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { db } from "../../firebase";
@@ -8,26 +8,61 @@ export const NoteList = () => {
 
     const params = useParams();
     console.log(params.id);
-    console.log(window.location.href);
+    const subject = window.location.href.split('/')[4];
+    console.log(window.location.href.split('/')[4]);
     const [note, setNote] = useState(['']);
+    const [object, setObject] = useState([])
+
+    const getCurrentDoc = async (n: any)  => {
+        const obj: object | any = {};
+        let topics: any = [];
+        let ids: any = [];
+        const querySnapshot = await getDocs(collection(db, `/Subjects/${subject}/Topics`));
+        querySnapshot.docs.forEach((doc) => {
+                    
+                    ids.push(doc.id);
+                    topics.push(doc.data().Topic)
+                })
+                topics.forEach((element:any, index: any) => {
+                    obj[element] = ids[index];
+                });
+            
+                console.log(obj)
+                console.log(typeof obj[n]);
+                
+                return obj[n];
+                 
+    }
+
+    useEffect(() => {
+        getCurrentDoc(params.id)
+        .then((data: any) => setObject(data))
+        console.log(object);
+    }, [])
+    console.log(object);
+    
     
     useEffect(() => {
-        getDocs(collection(db, `/Subjects/Biologia/Topics/50Sjaik2II690AWB1bIf/Notes`)).then((querySnapshot) => {
-            let topics: string[] = [];
-             querySnapshot.forEach((doc) => {
-                topics.push(doc.data().Note);
-             })
-             setNote(topics);
-          })
-        console.log(params.id);
-          
-       }, [])
+        const downloadData = async () => {
+            let notes: string[] = [];
+            const querySnapshot = await getDocs(collection(db, `/Subjects/${subject}/Topics/${object}/Notes`));
+            querySnapshot.docs.forEach((doc) => {
+                notes.push(doc.data().Note);
+            })
+    
+             setNote(notes);
+            console.log(params.id);
+        }
+        downloadData();
+        console.log('useEffect');
+        
+       }, [object])
     return (
         <>
             <Nav/>
             <div>Notatki z tematu: {params.id}</div>
-            {note.map((note) => (
-                <div>{note}</div>
+            {note.map((note, number) => (
+                <div key={number}>{note}</div>
             ))}
         </>
         
