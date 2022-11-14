15c86100750d31f2e59711908753c1512534d2a5
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-14 10:59:18 +0100

    Fix notes downloading is working

--- hs-notes/src/components/NoteList/NoteList.tsx
+++ hs-notes/src/components/NoteList/NoteList.tsx
@@ -1,4 +1,7 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { db } from "../../firebase";
 import { Nav } from "../Nav/Nav"
 
 export const NoteList = () => {
@@ -6,12 +9,26 @@ export const NoteList = () => {
     const params = useParams();
     console.log(params.id);
     console.log(window.location.href);
+    const [note, setNote] = useState(['']);
     
-    
+    useEffect(() => {
+        getDocs(collection(db, `/Subjects/Biologia/Topics/50Sjaik2II690AWB1bIf/Notes`)).then((querySnapshot) => {
+            let topics: string[] = [];
+             querySnapshot.forEach((doc) => {
+                topics.push(doc.data().Note);
+             })
+             setNote(topics);
+          })
+        console.log(params.id);
+          
+       }, [])
     return (
         <>
             <Nav/>
             <div>Notatki z tematu: {params.id}</div>
+            {note.map((note) => (
+                <div>{note}</div>
+            ))}
         </>
         
     )
