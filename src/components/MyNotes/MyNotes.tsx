48c08916db1f9bbf707359003d48ba0629dcc55c
Beata Gonera <beataiwonagonera@gmail.com>
2022-11-16 15:07:10 +0100

    removing note from my notes

--- src/components/MyNotes/MyNotes.tsx
+++ src/components/MyNotes/MyNotes.tsx
@@ -3,10 +3,11 @@ import '../MyNotes/MyNotes.style.css'
 import { TabsSubjects } from '../TabsSubjects/TabsSubjects';
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
 import { db, auth } from '../../firebase'
 import { SubjectNotes } from '../SubjectNotes/SubjectNotes';
 import {Link, useNavigate} from 'react-router-dom';
+
 
 interface MyNotesInterface{
     Author: string;
@@ -25,6 +26,7 @@ export const MyNotes = () => {
     const user = auth.currentUser;
     const [url, setUrl] = useState('')
     const [myNotes, setMyNotes] = useState<MyNotesInterface[]>([])
+    const [noteToBeDeleted, setNoteToBeDeleted] = useState('')
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -51,11 +53,7 @@ export const MyNotes = () => {
         })
     },[])
 
-    const removeFromMyNotes = () => {
 
-    }
-
-   
     return ( 
         <div>
             <Nav/>
@@ -63,7 +61,9 @@ export const MyNotes = () => {
             <div className='div-my-notes-container'>
                 {/* {url !== '' && <div className='div-notes-card'><img src={url}></img></div>} */}
                 {myNotes && myNotes.map((note)=> (
-                    <div key={note.ID} className="div-notes-card" onClick={()=>navigate(`/subjects/${note.Subject}/${note.Topic}/${note}`)}>
+                    <div key={note.ID} className="div-notes-card" 
+                    // onClick={()=>navigate(`/subjects/${note.Subject}/${note.Topic}/${note}`)}
+                    >
                         <div className='note-details'>
                             <div className='topic'>Temat: {note.Note}</div>
                             <div>Autor: {note.Author}</div>
@@ -72,7 +72,11 @@ export const MyNotes = () => {
                         <div className='note-buttons'>
                             <button>Przeglądaj fiszki</button>
                             <button>Zrób test</button>
-                            <button className='remove-note-button' onClick={removeFromMyNotes}>Usuń z moich notatek</button>
+                            <button className='remove-note-button' 
+                                    onClick={async ()=>(
+                                        deleteDoc(doc(db, `${user?.email}`, `${note.Note}`))
+                                    )}
+                            >Usuń z moich notatek</button>
                         </div>
                     </div>
                  ))}
